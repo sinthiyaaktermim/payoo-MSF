bb3e1a320986e41a169ee07c3a2af520ad1580fd
Sinthiya Akter Mim <info.sinthiyaaktermim@gmail.com>
2024-11-09 22:54:05 +0600

    added simple login handler and prevent defualt

---
+++ js/login.js
@@ -0,0 +1,7 @@
+// console.log('added somthing');
+document.getElementById('button-login').addEventListener('click', function (e) {
+  e.preventDefault();
+ console.log('login button control');
+ const phoneNumber = document.getElementById('PHONE-NUMBER');
+ console.log(phoneNumber);
+});
