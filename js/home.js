1f826a9eaece0f1fb6ed3cc129f82bb9a669efb6
Sinthiya Akter Mim <info.sinthiyaaktermim@gmail.com>
2024-11-10 10:19:43 +0600

    added event money handler

---
+++ js/home.js
@@ -0,0 +1,6 @@
+document.getElementById('button-add-money').addEventListener('click', function(e){
+ e.preventDefault();
+
+ const addMoney = document.getElementById('Amount-number').value;
+ const pinNumber = document.getElementById('pin-number').value;
+})
--- js/login.js
+++ js/login.js
@@ -1,13 +1,16 @@
 // console.log('added somthing');
 document.getElementById('button-login').addEventListener('click', function (e) {
   e.preventDefault();
-  const phoneNumber = document.getElementById('PHONE-NUMBER').value;
+  const phoneNumber = document.getElementById('phone-number').value;
   const pinNumber = document.getElementById('pin-number').value;
  
  // bad way to vailded
  if (phoneNumber === '5' && pinNumber === '1234') {
    console.log('you are log in');
-   window.location.herf = '/home.html';
+   // window.location.herf = '/home.html';
+    window.location.href = 'home.html';
+  
+
  } else {
    alert('enter your right pin and phonenumber')
  }
