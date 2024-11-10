0e3945c3bb8b89f92f55d9ac6c73d82e85286dce
Sinthiya Akter Mim <info.sinthiyaaktermim@gmail.com>
2024-11-10 10:38:51 +0600

    adding new balanace

--- js/home.js
+++ js/home.js
@@ -3,4 +3,17 @@ document.getElementById('button-add-money').addEventListener('click', function(e
 
  const addMoney = document.getElementById('Amount-number').value;
  const pinNumber = document.getElementById('pin-number').value;
+ if (pinNumber === '1234') {
+  console.log('adding money to your account');
+  const balance = document.getElementById('account-balance').innerText;
+  const addNumber = parseFloat(addMoney);
+  // console.log(addNumber);
+  const blanaceNumber = parseFloat(balance);
+  // console.log(blanaceNumber);
+  const newBalance = addNumber + blanaceNumber;
+  // console.log(newBalance);
+  document.getElementById('account-balance').innerText = newBalance;
+ } else {
+  alert('failed to add money')
+ }
 })
