03db7ed85f8f96c3b0e435e4bf245f40df17462c
Sinthiya Akter Mim <info.sinthiyaaktermim@gmail.com>
2024-11-10 09:21:09 +0600

    added home page design

--- js/login.js
+++ js/login.js
@@ -1,7 +1,14 @@
 // console.log('added somthing');
 document.getElementById('button-login').addEventListener('click', function (e) {
   e.preventDefault();
- console.log('login button control');
- const phoneNumber = document.getElementById('PHONE-NUMBER');
- console.log(phoneNumber);
+  const phoneNumber = document.getElementById('PHONE-NUMBER').value;
+  const pinNumber = document.getElementById('pin-number').value;
+ 
+ // bad way to vailded
+ if (phoneNumber === '5' && pinNumber === '1234') {
+   console.log('you are log in');
+   window.location.herf = '/home.html';
+ } else {
+   alert('enter your right pin and phonenumber')
+ }
 });
